b62e312d7908aae5a9d6f45c7c40ec188a4100e9
James <jfourie@bu.edu>
2025-05-21 12:40:05 -0400

    companies, excel file

---
+++ companies.py
@@ -0,0 +1,12 @@
+sector_designations = {'PSCI':'Index', 'PSCT':'Index', 'PSCM':'Index', 'PSCU':'Index',
+        'RSPG':'Index', 'RSPF':'Index', 'MITK':'Tech','NXT':'Tech', 'PATK':
+        'Industrials', 'PECO':'Real Estate', 'PUBM':'Tech', 'STEP':'Financial',
+        'ENSG':'Healthcare', 'MODG':'Consumer','WINA':'Consumer', 'AGCO':'Industrials',
+        'BMI':'Industrials', 'SKY':'Consumer', 'CVLT':'Tech', 'EHC':'Healthcare',
+        'INMD':'Healthcare', 'PSR':'Index', 'KBWR':'Index', 'PSCC':'Index', 'PSCH':'Index'}
+
+        
+companies = ['PSCI', 'PSCT', 'PSCM', 'PSCU', 'RSPG', 'RSPF', 'MITK',
+            'NXT', 'PATK', 'PECO', 'PUBM', 'STEP', 'ENSG', 'MODG', 
+            'WINA', 'AGCO', 'BMI', 'SKY', 'CVLT', 'EHC',  'INMD', 
+            'PSR',  'KBWR', 'PSCC', 'PSCH']
