ce333070d694f1fcd8ab313b12a342a1ed0917b0
Anjali Amin <113141108+AnjaliAmin@users.noreply.github.com>
2026-01-25 23:46:45 -0500

    Update application.py

--- application.py
+++ application.py
@@ -30,7 +30,7 @@ from dashboard_utils import (
 Fetch Data 
 """
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 filepath = os.path.join(BASE_DIR, "BUFC_May_2025_Allocations.xlsx")
 
 def load_data():
